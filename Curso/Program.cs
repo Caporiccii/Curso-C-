00bbdd9af9a3013d4010e6cc523abbb36e255c17
Marco David Coelho <marco.coelho@easynvest.com.br>
2020-05-12 20:23:45 -0300

    Add project files.

---
+++ Curso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Curso ae né!");
+        }
+    }
+}
