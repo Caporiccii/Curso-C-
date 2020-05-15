d28893b2acf2bba7c4a55a46e2548ef57c7f8e16
Marco David Coelho <marco.coelho@easynvest.com.br>
2020-05-15 18:51:24 -0300

    Test Pipeline data

---
+++ Curso/ExerciciosLogica/Exercicio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso
+{
+    public class Exercicio1
+    {
+          
+        double testNumero = 123.456;
+
+        public double Soma()
+        {
+            Console.WriteLine(Math.Round(testNumero, 2));
+
+            return testNumero;
+        }
+        
+    }
+}
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Curso.TestPipeline;
+using System;
 
 namespace Curso
 {
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Curso ae né!");
+        {       
+        Exercicio1 ex = new Exercicio1();        
+        ex.Soma();
+
+        MembersPipe mp = new MembersPipe();
+
+            mp.DownloadString();
+            mp.CreatWordList();
+            mp.FilterWordList();
+            mp.FindReverseWord();
+            mp.Print();
+          
         }
     }
 }
+ 
