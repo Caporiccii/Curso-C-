fd64735d49e541b61edb8d857c5a5fb7dbd35b77
Marco David Coelho <marco.coelho@easynvest.com.br>
2020-08-24 16:02:53 -0300

    aula 19 section 3

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,28 +1,40 @@
 ﻿using Curso.ExerciciosLogica;
 using Curso.TestPipeline;
 using System;
+using System.Diagnostics;
 
 namespace Curso
 {
     public class Program
     {
         static void Main(string[] args)
-        {       
+        {
+            Stopwatch stopwatch = new Stopwatch();
         Exercicio1 ex = new Exercicio1();        
         ex.Soma();
 
         SaidaDeDados saida = new SaidaDeDados();
-
+            stopwatch.Start();
             saida.EscreveTextoExercicio();
+            stopwatch.Stop();
 
-          
-        //MembersPipe mp = new MembersPipe();
+
+            Console.WriteLine($"Executado em {stopwatch.ElapsedMilliseconds} milisegundos");
+           //test//
+            int a = 6;
+            a *= 2;
+            Console.WriteLine("Atribuição " + a );
+            //fim test//
+
+
+            //MembersPipe mp = new MembersPipe();
 
             //    mp.DownloadString();
             //    mp.CreatWordList();
             //    mp.FilterWordList();
             //    mp.FindReverseWord();
             //    mp.Print();
+
 
         }
     }
